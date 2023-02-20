74f9061c8341b2d886171e04dee97e70b5efefb1
mariobalrod <marioballestero.rdg@gmail.com>
2023-02-20 11:47:57 +0700

    feat(components): add text component

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,6 +1,23 @@
+import { forwardRef } from 'react';
+
 import { Container } from './styles';
 import { Props } from './types';
 
-export function Text({ children, className }: Props) {
-  return <Container className={className}>{children}</Container>;
-}
+export const Text = forwardRef<HTMLElement, Props>(function TextComponent(
+  { tag, weight, variant, color, children, className, ...props },
+  ref,
+) {
+  return (
+    <Container
+      ref={ref}
+      as={tag}
+      $color={color}
+      $weight={weight}
+      $variant={variant}
+      className={className}
+      {...props}
+    >
+      {children}
+    </Container>
+  );
+});
